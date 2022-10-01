1c20bdc1920d335ed9836d215e1bf150391215bb
Bhavya Kalra <bkalra08032002@gmail.com>
2022-10-01 14:24:18 +0530

    Initial commit

---
+++ src/main/java/com/BookingAPIUser/UserBookingAPI/UserBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.BookingAPIUser.UserBookingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BookingAPIUser/UserBookingAPI/UserBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookingAPIUser.UserBookingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
